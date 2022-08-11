da0cb7614f0393f09c6ed7c1f722d676d668ebf2
Dehui Yu <dyu105@uottawa.ca>
2022-08-10 22:51:28 -0400

    Generating a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,7 +3,7 @@ import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from '../models/user';
 import { BadRequestError } from "../errors/Bad-Request-Error";
-
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -32,7 +32,21 @@ router.post(
 
     const user = User.build({email,password});
     await user.save();
+
+    //Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, 'asdf');
+
+    //Store it on session Object
+    req.session = {
+      jwt: userJwt
+    };
+
+
     res.status(201).send(user);
+
 
   }
 );
