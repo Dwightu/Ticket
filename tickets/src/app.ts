3a07117c91051aaeab0ffcc4516cfe8a267e4fcd
Dehui Yu <dyu105@uottawa.ca>
2022-08-13 23:52:46 -0400

    Finish Ticket Service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,31 +1,31 @@
-//Testing
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-
-import { errorHandler,currentUser } from '@dehui/common';
-import { NotFoundError } from '@dehui/common';
-import { createTicketRouter } from './routes/new';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@dehui/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false,
-        // it means that cookies are only to be shared when 
-        // someone is making a request to our server via http request
-        secure: process.env.NODE_ENV !== 'test'
-    })
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
 );
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
