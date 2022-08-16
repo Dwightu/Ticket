43bc2894473ecfc5aabe430f11edab54d9edb8e1
Dehui Yu <dyu105@uottawa.ca>
2022-08-15 20:26:29 -0400

    Publish Events

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,7 +5,7 @@ const stan = nats.connect('ticketing','abc',{
     url: 'http://localhost:4222',
 });
 
-stan.on('connect',()=>{
+stan.on('connect',async()=>{
     console.log('Publisher connected to nats')
     // const data = JSON.stringify({
     //     id: '123',
@@ -18,9 +18,14 @@ stan.on('connect',()=>{
     //   });
 
     const publisher=new TicketCreatedPublisher(stan);
-    publisher.publish({
-      id:'123',
-      title:'concert',
-      price:20
-    })
+    
+    try {
+      await publisher.publish({
+        id: '123',
+        title: 'concert',
+        price: 20,
+      });
+    } catch (err) {
+      console.error(err);
+    }
 })
