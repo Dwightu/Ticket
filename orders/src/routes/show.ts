2b964f1153e61a087588624c7972c56ff3bc40b1
Dehui Yu <dyu105@uottawa.ca>
2022-08-26 00:45:41 -0400

    Completed Order Service

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,28 @@
-import express,{Request,Response} from 'express';
-const router=express.Router();
+import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@dehui/common";
+import { Order } from "../models/order";
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
+const router = express.Router();
 
-    res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
