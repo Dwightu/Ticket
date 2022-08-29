0c2972a024345b63591040da51b1c4088110cb1e
Dehui Yu <dyu105@uottawa.ca>
2022-08-29 00:50:36 -0400

    Try

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log('Test CI/CD Third time')
+  console.log('Test CI/CD Third time...')
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
